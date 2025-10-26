178970fac68805182272399cc5f92c33a99067dc
JEJ07 <jjohnemman@gmail.com>
2025-10-26 23:37:48 +0800

    fixed the bug on register form

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterForm(UserCreationForm):
     password1 = forms.CharField(
@@ -22,9 +23,10 @@ class RegisterForm(UserCreationForm):
         password2 = self.cleaned_data.get("password2")
 
         if password1 and password2 and password1 != password2:
-            self.add_error("password1", "Passwords do not match.")
-            self.add_error("password2", "Passwords do not match.")
-            return password2
+            raise forms.ValidationError("Passwords do not match.")
 
-        return super().clean_password2()  # Django strength validation
+        
+        validate_password(password2, self.instance)
+
+        return password2
     
