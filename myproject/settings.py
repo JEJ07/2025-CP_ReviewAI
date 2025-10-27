f4ae5a530beda46e911d493c2e9dd125d3dac078
phrxnc <franckaming2003@gmail.com>
2025-10-27 19:07:15 +0800

    email register field and forgot password

--- myproject/settings.py
+++ myproject/settings.py
@@ -204,3 +204,15 @@ MEDIA_ROOT = BASE_DIR / 'reviewai' / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'franckaming2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'pltw ncrm ewfi mozh'
+DEFAULT_FROM_EMAIL = 'ReviewAI <franckaming2003@gmail.com>'
+
+# Password Reset Settings
+PASSWORD_RESET_TIMEOUT = 3600
--- users/urls.py
+++ users/urls.py
@@ -14,4 +14,32 @@ urlpatterns = [
     
     path("update-session-preference/", views.update_session_preference, name="update_session_preference"),
 
+
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html',
+             email_template_name='users/password_reset_email.txt',
+             subject_template_name='users/password_reset_subject.txt',
+             success_url='/users/password-reset/done/'
+         ), 
+         name='password_reset'),
+    
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ), 
+         name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html',
+             success_url='/users/reset/done/'
+         ), 
+         name='password_reset_confirm'),
+    
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'),
 ]
